e821b797911e5790c488d3c4d09f91031c7750a3
Liuba <pl@mail.ru>
2022-09-26 21:47:53 +0300

    Task 1

--- ZD25/Program.cs
+++ ZD25/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+Console.Clear();
+Console.Write("Введите основание степени A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите показатель степени B: ");
+int b;
+while (true)
+{
+   b = Convert.ToInt32(Console.ReadLine());
+   if (b > 0)
+   {
+      break;
+   }
+   Console.WriteLine("Введите положительное число B: ");
+}
+
+
+int z = Convert.ToInt32(Math.Pow(a, b));
+Console.Write(a + " в степени " + b + " = " + z);
