3a544386006192ff323ec63fea86cd2a1eb927c5
Liuba <pl@mail.ru>
2022-09-28 22:43:25 +0300

    Task 2

--- ZD27/Program.cs
+++ ZD27/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string a = Convert.ToString(n);
+int[] Array = new int[a.Length];
+int s = 0;
+for (int i = 0; i < a.Length; i++) 
+{
+   Array[i] = int.Parse(a[i].ToString());
+   s = s + Array[i];
+}
+
+Console.WriteLine(s);
