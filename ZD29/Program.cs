64abb34c576d13a579a6e1db2a9a2ea0d8639358
Liuba <pl@mail.ru>
2022-09-30 21:50:11 +0300

    Task 3

--- ZD29/Program.cs
+++ ZD29/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+Console.Clear();
+
+int[] myArray = new int[8];
+
+{
+   for (int i = 0; i < 8; i++)
+{
+   Console.Write($"Введите элемент массива № {i}: ");
+   myArray[i] = int.Parse(Console.ReadLine());
+
+}
+}
+
+Console.WriteLine("Вывод массива: ");
+Console.WriteLine("[" + string.Join(", ", myArray) + "]");
